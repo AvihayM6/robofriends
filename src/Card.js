14732774b4e221ac437a04d22aa5c5432db40dd2
avihay <avihaymaman1234@gmail.com>
2021-07-27 01:22:05 +0300

    First commit and init

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () =>{
+    return(
+        <div>
+            <img alt='robots' src='https://robohash.org/sdad' />
+            <div>
+                <h2>Robot 1</h2>
+                <p>robot1@gmail.com</p>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default Card;

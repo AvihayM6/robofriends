3991f7259504ff1f8053e34b042a90dfd5fc1916
avihay <avihaymaman1234@gmail.com>
2021-07-27 16:39:30 +0300

    add few components

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-template-curly-in-string */
 import React from 'react';
 
+/*Card component is produce a card that include img, h2, email for input from robots.js*/
 
 const Card = ({name, email, id}) =>{
     return(
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-green dib br4 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Card from './Card'
 
+
+/* CardList component is produce a Card component for each value in robots.js*/
+
 const CardList = ({robots}) =>{
     const cardComp = robots.map((user, i) =>{
-        return <Card name={robots[i].name} email={robots[i].email} id={robots[i].id}/>
+        return <Card key = {i} name={robots[i].name} email={robots[i].email} id={robots[i].id}/> //map should include return value
     })
     return(
-        <div>{cardComp}</div>
-    );
+        <div>
+            {cardComp}
+        </div> 
+    ); // the component return
 }
 
 export default CardList;

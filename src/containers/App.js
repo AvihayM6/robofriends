7428aea8c92237f3f3ca0d99ffcd3ab638c614a9
avihay <avihaymaman1234@gmail.com>
2021-07-29 01:27:42 +0300

    Write the React with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 
-class App extends Component{
-    constructor() {
-        super();
-        this.state = {
-            robots:[],
-            searchfield:''
-        }
-    }
+function App(){
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots:[],
+    //         searchfield:''
+    //     }
+    // }
+
+    //Instead the constructor
+    const [robots, setRobots] = useState([]); //arrays distructoring is allows us to naming the state([])
+    const [searchfield, setSearchfield] = useState(''); //arrays distructoring is allows us to naming the state([])
+
     /*Receive the information of the robots from API*/
-    componentDidMount(){
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({robots:users}))
+    // }
+    
+    //Instead the componentDidMount
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({robots:users}))
-    }
+        .then(users => setRobots(users))
+    }, [])
+
+
 
     /*This function is change the state of the searchfieald to be the current input of the searchfield*/
-    onSearchChange = (event)=>{
-        this.setState({searchfield: event.target.value}) //Take the text from searchfield textbox and save it on searchfield variable
+    const onSearchChange = (event)=>{
+        setSearchfield(event.target.value) //Take the text from searchfield textbox and save it on searchfield variable
     }
 
     /*Lines 22-24 is filtered the robots as the incoming text from the searchfield variable
     and return array of the robots are feets*/
-    render(){
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+    // render(){
+        const filteredRobots = robots.filter(robots =>{
+            return robots.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
         })
         return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <CardList robots={filteredRobots} /> {/* filteredRobots include the robots that match to the textfield input*/}
                 </Scroll>
             </div>
         );
     }
-    }
+    // }
 
 export default App;

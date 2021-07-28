bf97a755d6ebeb0b3ccd9c588c76872d30523b48
avihay <avihaymaman1234@gmail.com>
2021-07-28 04:30:42 +0300

    add style and more logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component{
     constructor() {
         super();
         this.state = {
-            robots:robots,
+            robots:[],
             searchfield:''
         }
+    }
+    /*Receive the information of the robots from API*/
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots:users}))
     }
 
     /*This function is change the state of the searchfieald to be the current input of the searchfield*/
     onSearchChange = (event)=>{
-        this.setState({searchfield: event.target.value})
+        this.setState({searchfield: event.target.value}) //Take the text from searchfield textbox and save it on searchfield variable
     }
 
-    /*Lines 22-24 is filtered the robots as the incoming text from the search field 
+    /*Lines 22-24 is filtered the robots as the incoming text from the searchfield variable
     and return array of the robots are feets*/
     render(){
         const filteredRobots = this.state.robots.filter(robots =>{
@@ -25,7 +31,7 @@ class App extends Component{
         })
         return(
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots} /> {/* filteredRobots include the robots that match to the textfield input*/}
             </div>
